a725ab89f4883411e973794f982735aee5853966
Bostos <nikolast_metal@hotmail.com>
2021-10-22 14:13:02 +0200

    fix

--- resources/views/pages/submit-paper.blade.php
+++ resources/views/pages/submit-paper.blade.php
@@ -4,7 +4,7 @@
     <x-slot name="description">{{ $page->description }}</x-slot>
     <x-slot name="keywords">{{ $page->keywords }}</x-slot>
 
-    <div class="mx-auto sm:px-6 lg:px-8 bg-white">
+    <div class="mx-auto sm:px-6 lg:px-8 bg-white min-h-screen">
         <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg py-5 p-5">
             <h1 class="text-2xl font-semibold mt-2 pb-4 py-2 text-yellow-500">{{ $page->title }}</h1>
             <hr>
